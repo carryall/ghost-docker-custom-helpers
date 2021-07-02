9ad680421076e50adbc7b1b65671cf21ce18341f
Carryall <carryall369@gmail.com>
2021-07-02 10:09:01 +0700

    Update path to support ghost 4.7

--- _tools/ghost.paths.js
+++ _tools/ghost.paths.js
@@ -5,5 +5,5 @@ const GHOST_CORE_PATH = `${GHOST_PATH}/core`;
 
 const GHOST_FRONTEND_PATH = `${GHOST_CORE_PATH}/frontend`;
 export const FRONTEND_HELPERS_TARGET_DIRECTORY = `${GHOST_FRONTEND_PATH}/helpers`;
-export const FRONTEND_HELPERS_REGISTRATION_FILE = `${GHOST_FRONTEND_PATH}/services/themes/handlebars/helpers.js`;
+export const FRONTEND_HELPERS_REGISTRATION_FILE = `${GHOST_FRONTEND_PATH}/services/themes-engine/handlebars/helpers.js`;
 export const GSCAN_KNOWN_HELPERS_TARGET_FILE = `${GHOST_PATH}/node_modules/gscan/lib/specs/canary.js`;
